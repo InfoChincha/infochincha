db40333eb5c992dcb0c08fbc697562573874495c
InfoChincha <63263834+InfoChincha@users.noreply.github.com>
2020-04-30 23:37:06 -0500

    d

---
+++ Nuevo_Proyecto/firebase/conexion.php
@@ -0,0 +1,20 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA279UaCt1HZ90keWl2W58ijd-9swi7RUk",
+    authDomain: "vela-f80ba.firebaseapp.com",
+    databaseURL: "https://vela-f80ba.firebaseio.com",
+    projectId: "vela-f80ba",
+    storageBucket: "vela-f80ba.appspot.com",
+    messagingSenderId: "135730375195",
+    appId: "1:135730375195:web:078bb05f2ce1b5d72666be"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
