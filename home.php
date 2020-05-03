5acd0d69fff40fead430edae1e8e984611a24b7c
InfoChincha <63263834+InfoChincha@users.noreply.github.com>
2020-05-03 12:01:03 -0500

    sc

---
+++ home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=7">
+    <link rel="stylesheet" href="canasta.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700;800&display=swap" rel="stylesheet">
+    <title>InfoChincha</title>
+    <link rel="icon" href="IMG/infochincha_icon.png">
+    <style type="text/css">
+    	*{overflow: hidden}
+     
+    
+
+    </style>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="textos">
+                <h1>SORTEO CANASTA DE VÍVERES 2020</h1>
+                <h2>InfoChincha</h2>
+                <a href="Noticias/Principal_Noticia.html">ENTRAR</a>
+            </div>
+            <img src="madre.png" alt="">
+            
+        </div>
+    </header>
+<!--     <div class="wave">
+        <div style="height: 150px; overflow: hidden;" >
+            <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+                <path d="M0.00,49.99 C150.00,150.00 349.20,-49.99 500.00,49.99 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #fff;">
+                    
+                </path>
+            </svg>
+        </div>
+    </div> -->
+</body>
+</html>
