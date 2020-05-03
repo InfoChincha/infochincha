e5f14087d41994423f8d4b0f44181e39aa54351c
InfoChincha <63263834+InfoChincha@users.noreply.github.com>
2020-05-03 17:14:04 -0500

    tabla
    sd

--- participante.php
+++ participante.php
@@ -15,6 +15,7 @@ if (!$link) {
 <html>
 <head>
 	<title></title>
+	<link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
 	 <style type="text/css">
     	/*body{overflow: hidden}*/
         #cuadro{
@@ -45,8 +46,6 @@ if (!$link) {
             0%{
                 left:-100px;
                
-                
-               
             }
             50%{
             	 background-color:white;
@@ -57,89 +56,99 @@ if (!$link) {
                 
             }
         }
+
         #CENTRAL{
-        	background: blue; 
-        	width:1480px;
+        	position: relative;
+        	background: white; 
+        	width:1380px;
         	margin:0 auto;
+        	top:130px;
         }
         #tarjeta{
-        	background: red;
+        	background: #4100f7;
+        	
         	position: relative;
-        	width: 200px;
-        	height: 200px;
+        	width: 150px;
+        	height: 150px;
         	float:left;
         	border-radius:8px;
-        	margin-left:10px;
-        	margin-bottom:10px;
+        	margin-left:20px;
+        	margin-bottom:20px;
         }
         #nombre{
         	position: relative;
         	background: white;
-        	color:blue;
+        	color:#808080;
         	border-radius: 8px;
         	text-align: center;
-        	width: 90%;
-        	height: 40px;
-        	top:75%;
-        	margin:0px auto;
+        	width: 100%;
+        	height: 100%;
+        	
   			display: table;
    			align-items: center;
    			text-align:center;
-   			
-
+   			line-height:150px;
         }
+        
+       
         #nombre p{
-        	display:inline-block
-        	vertical-align: middle;
+        	
+        display:inline-block;
+		vertical-align:middle;
+		line-height:normal;
+		font-size: 20px;
+		font-family: 'Raleway', sans-serif;
+        }
+
+        #nombre:hover{
+        	transition: opacity 0.3s;
+        	opacity: 0;
         }
         img{
         	position:absolute;
         	
-        	width: 200px;
-        	height: 200px;
+        	width: 150px;
+        	height: 150px;
         	display: table-cell;
         	vertical-align:middle;
     		text-align:center;
-    		border-radius: 8px;
+    		border-radius: 10px;
 
    
         	
         }
-        table{
-        	background: green;
-        	width:80%;
-        	height: 200px;
-        	margin:0 auto;
-        }
-        td{
-        	
-        	background:teal;
-        }
+
+
     </style>
 </head>
-<body><div id="CENTRAL" >
-<div id="cuadro">
+<body bgcolor="#4100f7">
+	<div id="cuadro">
         HOLI
    	</div> 
+	<div id="CENTRAL" >
+
    	<?php
    	$consulta= "SELECT * FROM participante";
 	$datos=mysqli_query($link,$consulta);
 while ($fila =mysqli_fetch_array($datos)) {
  	$ima=$fila['imagen'];
  	$name=$fila['nombres'];
+ 	$id=$fila['id'];
+
  	 echo"<div id='tarjeta'><img src='$ima';>
     		<div id='nombre' >
-    			<p>ddd</p>
+    			<p>$id </p>
+    			
     		</div>	
 		</div>";
 } 
-
+mysqli_close($link);
   ?>
 
 	
     
     <div id="tarjeta">
-	<img src="<?php echo $ima?>">
+	<img src="https://scontent-lim1-1.xx.fbcdn.net/v/t1.0-9/95957278_976209036128106_5483503863426187264_n.png?_nc_cat=102&_nc_sid=8024bb&_nc_eui2=AeFJrCEDoLvs0kxhLpjMGPjLNqdGq_iYXmY2p0ar-JheZpscoRO9wn49QxMqBn4UpAsNL_85hKISwwxanwfj9WPZ&_nc_ohc=NlQXmKgwd-QAX9pwrx3&_nc_ht=scontent-lim1-1.xx&oh=34b57c3265f8de6f3bfec2e9d560f4bb&oe=5ED54CBB">
     	<div id="nombre" >
     		<p>HOLA</p>
     	</div>	
