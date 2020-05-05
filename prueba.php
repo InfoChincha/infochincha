a0757ba803f54343a5162a24cec3e04ebcc5b767
InfoChincha <63263834+InfoChincha@users.noreply.github.com>
2020-05-05 00:14:11 -0500

    s

--- prueba.php
+++ prueba.php
@@ -1,2 +1,17 @@
 <?php 
-	echo rand(1,800); ?>
+$frase=array(
+	1=> "sdasd",
+	2=> "Hola",
+	3=> "Te amo",
+	4=> "Mi amor",
+	5=> "Cómo",
+	6=> "Estás",
+	7=> "Soy yo",
+	8=> "Quien te ama",
+	9=> "Cous",
+
+); 
+$numero =rand(1,9);
+echo "$frase[$numero]";
+	
+	?>
